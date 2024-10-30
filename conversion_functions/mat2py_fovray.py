382c6d3b587a50a2205d24de1d5fd4e80f3cdacd
ImmaFrancesca <imma.francesca.fiengo@estudiantat.upc.edu>
2024-10-30 11:36:22 +0100

    new mat2py functions

---
+++ conversion_functions/mat2py_fovray.py
@@ -0,0 +1,14 @@
+# This code implements the functions that call the SPICE function, adapting input and output
+# for our code.
+
+import spiceypy as spice
+
+# The function cspice_furnsh in MATLAB can receive:
+# - file: [n,c1] = size(file); char = class(file) OR [1,n] = size(file); cell = class(file)
+
+# The function spice.furnsh in Python receives:
+# - file: str or iterable(str)
+
+# The function cspice_furnsh in MATLAB gives no output.
+
+# The function spice.furnsh in Python gives no output:
